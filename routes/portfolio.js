85b637c6b22c2cd671b4fb01f7dd1e97c32006a8
naqash-smrt-is <naqash.ahmad@piyarse.com>
2020-10-25 18:13:04 +0500

    worked on portfolio (lesfloralies, lylwallet)

---
+++ routes/portfolio.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var router = express.Router();
+var path = require('path')
+
+/* GET portfolio listing. */
+router.get('/', function(req, res, next) {
+  res.sendFile('portfolio.html', {root: path.join(__dirname, '../public') })
+});
+
+/* GET lesfloralies listing. */
+router.get('/lesfloralies', function(req, res, next) {
+  res.sendFile('lesfloralies.html', {root: path.join(__dirname, '../public/portfolio') })
+});
+
+/* GET lesfloralies listing. */
+router.get('/loyalwallet', function(req, res, next) {
+  res.sendFile('loyalwallet.html', {root: path.join(__dirname, '../public/portfolio') })
+});
+
+module.exports = router;
